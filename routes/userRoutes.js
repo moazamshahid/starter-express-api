6f5d11d6c0dfd1ed5c2c5cefa0a362c095120fcf
Sharjeel Anjum <sharjeelanjum12@gmail.com>
2024-01-19 17:46:46 +0500

    sharjeelcomit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* Serve the index.html file */
 router.get('/file', function(req, res, next) {
-  res.sendFile('index.html', { root: __dirname + './index.html' });
+  res.sendFile('index.html', { root: __dirname + './' });
 });
 
 /* Other routes go here... */
